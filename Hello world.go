0a86299c505cc83db13df855b2784b7ebdfd3aa3
osmane <osmane.neto@hotmail.com>
2025-01-13 19:29:40 -0300

    .

---
+++ Hello world.go
@@ -0,0 +1,25 @@
+// You can edit this code!
+// Click here and start typing.
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+var a string = "Não me interesso por palhaçada"
+var b, c int = 1, 2
+
+const d = 100
+const e = 98898989 / d
+
+func main() {
+
+	fmt.Println("Hello, \n Elon musk \tSecretária", a)
+	fmt.Println(b, "\n", c)
+	fmt.Println("\n", d)
+	fmt.Println("\n", e*100)
+	fmt.Println(int64(e))
+	fmt.Println(math.Sin(d))
+
+}
